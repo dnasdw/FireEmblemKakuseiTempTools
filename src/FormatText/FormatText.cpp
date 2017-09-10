d7bdb94d1385dbfd186e6c7201b2422cb2ea73a0
Sun Daowen <sundaowen@gmail.com>
2017-09-11 00:16:05 +0800

    init FormatText and CheckOrigText

---
+++ src/FormatText/FormatText.cpp
@@ -0,0 +1,79 @@
+#include <sdw.h>
+
+int UMain(int argc, UChar* argv[])
+{
+	if (argc != 2)
+	{
+		return 1;
+	}
+	FILE* fp = UFopen(argv[1], USTR("rb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	u32 uTxtSize = ftell(fp);
+	if (uTxtSize % 2 != 0)
+	{
+		fclose(fp);
+		return 1;
+	}
+	uTxtSize /= 2;
+	fseek(fp, 0, SEEK_SET);
+	Char16_t* pTemp = new Char16_t[uTxtSize + 1];
+	fread(pTemp, 2, uTxtSize, fp);
+	fclose(fp);
+	if (pTemp[0] != 0xFEFF)
+	{
+		delete[] pTemp;
+		return 1;
+	}
+	pTemp[uTxtSize] = 0;
+	wstring sTxt = U16ToW(pTemp + 1);
+	delete[] pTemp;
+	wstring sTxtNew;
+	wstring::size_type uPos0 = 0;
+	while ((uPos0 = sTxt.find(L"No.", uPos0)) != wstring::npos)
+	{
+		wstring::size_type uPos1 = sTxt.find(L"\r\n--------------------------------------\r\n", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sNum = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n--------------------------------------\r\n");
+		uPos1 = sTxt.find(L"\r\n======================================\r\n", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sStmtOld = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n======================================\r\n");
+		uPos1 = sTxt.find(L"\r\n--------------------------------------", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sStmtNew = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n--------------------------------------");
+		if (!sTxtNew.empty())
+		{
+			sTxtNew += L"\r\n\r\n";
+		}
+		sTxtNew += sNum;
+		sTxtNew += L"\r\n--------------------------------------\r\n";
+		sTxtNew += sStmtOld;
+		sTxtNew += L"\r\n======================================\r\n";
+		sTxtNew += sStmtNew;
+		sTxtNew += L"\r\n--------------------------------------\r\n";
+	}
+	fp = UFopen(argv[1], USTR("wb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fwrite("\xFF\xFE", 2, 1, fp);
+	fwrite(sTxtNew.c_str(), 2, sTxtNew.size(), fp);
+	fclose(fp);
+	return 0;
+}
