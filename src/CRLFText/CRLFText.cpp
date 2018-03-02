e1450402232d8e35b456e348961d26e866122c87
Sun Daowen <sundaowen@gmail.com>
2018-03-02 11:20:58 +0800

    init CRLFText

---
+++ src/CRLFText/CRLFText.cpp
@@ -0,0 +1,46 @@
+#include <sdw.h>
+
+int UMain(int argc, UChar* argv[])
+{
+	if (argc != 2)
+	{
+		return 1;
+	}
+	FILE* fp = UFopen(argv[1], USTR("rb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	u32 uTxtSize = ftell(fp);
+	if (uTxtSize % 2 != 0)
+	{
+		fclose(fp);
+		return 1;
+	}
+	uTxtSize /= 2;
+	fseek(fp, 0, SEEK_SET);
+	Char16_t* pTemp = new Char16_t[uTxtSize + 1];
+	fread(pTemp, 2, uTxtSize, fp);
+	fclose(fp);
+	if (pTemp[0] != 0xFEFF)
+	{
+		delete[] pTemp;
+		return 1;
+	}
+	pTemp[uTxtSize] = 0;
+	wstring sTxt = U16ToW(pTemp + 1);
+	delete[] pTemp;
+	sTxt = Replace(sTxt, L"\r\n", L"\n");
+	sTxt = Replace(sTxt, L'\r', L'\n');
+	sTxt = Replace(sTxt, L'\n', L"\r\n");
+	fp = UFopen(argv[1], USTR("wb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fwrite("\xFF\xFE", 2, 1, fp);
+	fwrite(sTxt.c_str(), 2, sTxt.size(), fp);
+	fclose(fp);
+	return 0;
+}
