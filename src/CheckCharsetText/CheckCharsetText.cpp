527dbafd602d1db012d342da8fe19e6409f48d96
Sun Daowen <sundaowen@gmail.com>
2018-03-06 10:06:33 +0800

    init CheckCharsetText

---
+++ src/CheckCharsetText/CheckCharsetText.cpp
@@ -0,0 +1,106 @@
+#include <sdw.h>
+
+int UMain(int argc, UChar* argv[])
+{
+	if (argc != 3)
+	{
+		return 1;
+	}
+	FILE* fp = UFopen(argv[1], USTR("rb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	u32 uTxtSize = ftell(fp);
+	if (uTxtSize % 2 != 0)
+	{
+		fclose(fp);
+		return 1;
+	}
+	uTxtSize /= 2;
+	fseek(fp, 0, SEEK_SET);
+	Char16_t* pTemp = new Char16_t[uTxtSize + 1];
+	fread(pTemp, 2, uTxtSize, fp);
+	fclose(fp);
+	if (pTemp[0] != 0xFEFF)
+	{
+		delete[] pTemp;
+		return 1;
+	}
+	pTemp[uTxtSize] = 0;
+	wstring sTxt = U16ToW(pTemp + 1);
+	delete[] pTemp;
+	wstring sCharset = L"\r\n";
+	for (u32 i = 0; i < static_cast<u32>(sTxt.size()); i++)
+	{
+		wchar_t uUnicode = sTxt[i];
+		if (uUnicode >= 0x20)
+		{
+			sCharset += uUnicode;
+		}
+	}
+	fp = UFopen(argv[2], USTR("rb"), false);
+	if (fp == nullptr)
+	{
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	uTxtSize = ftell(fp);
+	if (uTxtSize % 2 != 0)
+	{
+		fclose(fp);
+		return 1;
+	}
+	uTxtSize /= 2;
+	fseek(fp, 0, SEEK_SET);
+	pTemp = new Char16_t[uTxtSize + 1];
+	fread(pTemp, 2, uTxtSize, fp);
+	fclose(fp);
+	if (pTemp[0] != 0xFEFF)
+	{
+		delete[] pTemp;
+		return 1;
+	}
+	pTemp[uTxtSize] = 0;
+	sTxt = U16ToW(pTemp + 1);
+	delete[] pTemp;
+	wstring sTxtNew;
+	wstring::size_type uPos0 = 0;
+	while ((uPos0 = sTxt.find(L"No.", uPos0)) != wstring::npos)
+	{
+		wstring::size_type uPos1 = sTxt.find(L"\r\n--------------------------------------\r\n", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sNum = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n--------------------------------------\r\n");
+		uPos1 = sTxt.find(L"\r\n======================================\r\n", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sStmtOld = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n======================================\r\n");
+		uPos1 = sTxt.find(L"\r\n--------------------------------------", uPos0);
+		if (uPos1 == wstring::npos)
+		{
+			return 1;
+		}
+		wstring sStmtNew = sTxt.substr(uPos0, uPos1 - uPos0);
+		uPos0 = uPos1 + wcslen(L"\r\n--------------------------------------");
+		if (!sTxtNew.empty())
+		{
+			sTxtNew += L"\r\n\r\n";
+		}
+		wstring::size_type uPos = sStmtNew.find_first_not_of(sCharset);
+		if (uPos != wstring::npos)
+		{
+			wstring sWord = sStmtNew.substr(uPos, 1);
+			sStmtNew = Replace(sStmtNew, L"\r\n", L"\n");
+			UPrintf(USTR("%") PRIUS USTR(" |%") PRIUS USTR("|\n%") PRIUS USTR("\n"), WToU(sNum).c_str(), WToU(sWord).c_str(), WToU(sStmtNew).c_str());
+		}
+	}
+	return 0;
+}
